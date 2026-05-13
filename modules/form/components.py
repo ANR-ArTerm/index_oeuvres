ba841ef02bd7cbfdd8762b122e179a23e0441138
Pierrousson <pierre.husson.56@gmail.com>
2026-05-13 15:31:51 +0200

    ajout notice BouclierAchille par Pierre 2026-05-13T15:31:51.193036

--- modules/form/components.py
+++ modules/form/components.py
@@ -2,7 +2,13 @@ import streamlit as st
 
 def exemple_desc_image():
     with st.expander("Voir des exemples pour les droits"):
-        st.write('''
-                        Wikimedia Commons, CC BY-SA 4.0, Auteur de l'image (lien de l'image)
-                        Wikimedia Commons, CC BY-SA 4.0, Akinator (https://commons.wikimedia.org/wiki/File:Akinator.svg?lang=fr)
+        st.markdown('''
+                    #### Wikimedia
+                    Wikimedia Commons, CC BY-SA 4.0, Akinator (https://commons.wikimedia.org/wiki/File:Akinator.svg?lang=fr)
+                    
+                    #### Gallica
+                    `Source gallica.bnf.fr / Bibliothèque nationale de France`
+
+                    #### Metropolitan Museum
+                    `New York, Metropolitan Museum of Arts, inv. 31.31.24 (https://www.metmuseum.org/art/collection/search/342798)`
                     ''')
