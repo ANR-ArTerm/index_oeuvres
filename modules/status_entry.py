de51a989eacba1cb54d8a57c59f26669ecfd844d
Tricon-Carla <carla.tricon@gmail.com>
2026-06-22 21:10:37 +0200

    chang' application

--- modules/status_entry.py
+++ modules/status_entry.py
@@ -2,5 +2,6 @@ STATUS_ENTRY_OPTIONS = {
         0: "🔴 Notice non contrôlée (niv.0)",
         1: "📝 Manque la bibliographie (niv.1)",
         2: "Manque l'image",
+        3: "Carla pour la biblio ",
         8: "✅ Notice terminée (niv.8)",
     }
